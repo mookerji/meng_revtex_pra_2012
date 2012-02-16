7c992e94e134dff19e6dee0d59c16ac246988683
Bhaskar Mookerji <mookerji@mit.edu>
2012-02-16 17:07:07 -0500

    Started and populated repository for PRA publication.

---
+++ auto/meng_pra2012_mookerji_jhs.el
@@ -0,0 +1,78 @@
+(TeX-add-style-hook "meng_pra2012_mookerji_jhs"
+ (lambda ()
+    (LaTeX-add-bibliographies
+     "citations/proposal")
+    (LaTeX-add-labels
+     "chap:herald"
+     "sec:herald:overview"
+     "fig:channel_model"
+     "fig:channel_loss_model"
+     "eq:chap3:meas_povm"
+     "eqn:remaining_singlet"
+     "eqn:herald_prob"
+     "eq:success_prob_def"
+     "sec:herald:communication"
+     "fig:entanglement_connection"
+     "eqn:chap3:linear_beamsplitter"
+     "eqn:chap3:chia_orig"
+     "eq:scaled_vars"
+     "eq:char_final"
+     "eq:chap3:moments"
+     "eq:fourier_char"
+     "eq:chap3:trace_id"
+     "fig:chap3:con:heralding_prob"
+     "fig:chap3:con:fidelity"
+     "fig:chap3:con:ratio"
+     "fig:merit:entanglement_connection"
+     "eq:yx:moment"
+     "eq:yy:moment"
+     "eqn:fyx_nrpd"
+     "chap:conclusion")
+    (TeX-add-symbols
+     '("abs" 1)
+     '("soln" 1)
+     '("mcl" 1)
+     '("avga" 1)
+     '("avg" 1)
+     '("eqn" 1)
+     '("pnb" 1)
+     '("pna" 1)
+     '("parenb" 1)
+     '("parena" 1)
+     '("rd" 1)
+     '("braket" 2)
+     '("ket" 1)
+     '("bra" 1)
+     "sech"
+     "ad"
+     "bd"
+     "cda"
+     "cds")
+    (TeX-run-style-hooks
+     "subfigure"
+     "verbatim"
+     "hyperref"
+     "thumbpdf"
+     "bm"
+     "epsf"
+     "longtable"
+     "graphicx"
+     "pdftex"
+     "graphics"
+     "color"
+     "latex2e"
+     "revtex4-110"
+     "revtex4-1"
+     "aps"
+     "twocolumn"
+     "secnumarabic"
+     "amsmath"
+     "amssymb"
+     "pra"
+     "groupedaddress"
+     "showpacs"
+     "showkeys"
+     "spdc_dlcz_fin"
+     "spdc_dlcz_loss"
+     "ent_connection")))
+
